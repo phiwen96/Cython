ad96d573beb6a708c22f093788ded8008cd7eed6
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-22 12:36:01 +0100

    a

---
+++ app/inputfsm.hpp
@@ -0,0 +1,103 @@
+#pragma once
+using namespace std;
+
+namespace {
+struct Context;
+struct State
+{
+    virtual void process (char const* str, Context& ctx) {}
+};
+
+struct Context
+{
+    State* state {nullptr};
+    string input;
+    vector <string> outputs;
+    void process (char const* str);
+};
+
+
+//template <int N>
+struct Begin : State
+{
+    
+
+//    Begin (array<char const*, N>&& states)
+//    {
+//
+//    }
+    virtual void process (char const* str, Context& ctx);
+};
+
+struct Input : State
+{
+    virtual void process (char const* str, Context& ctx)
+    {
+//        cout << "Input::process" << endl;
+        ctx.input = str;
+        ctx.state = new Begin;
+//        delete this;
+    }
+};
+
+struct Output : State
+{
+    virtual void process (char const* str, Context& ctx)
+    {
+//        cout << "Output::process" << endl;
+        if (strcmp (str, "--input") == 0)
+        {
+            ctx.state = new Input;
+//            delete this;
+        } else
+        {
+            ctx.outputs.push_back (string {str});
+        }
+    }
+};
+
+void Context::process (char const* str)
+{
+//    cout << "Context::process" << endl;
+    state -> process (str, *this);
+}
+
+
+void Begin::process (char const* str, Context& ctx)
+{
+//    cout << "Begin::process" << endl;
+    if (strcmp (str, "--input") == 0)
+    {
+//        cout << "--input" << endl;
+        ctx.state = new Input;
+//        ctx.state = static_cast <Input*> (ctx.state);
+//        delete this;
+    } else if (strcmp (str, "--output") == 0)
+    {
+        ctx.state = new Output;
+    } else
+    {
+        throw runtime_error ("");
+    }
+}
+
+}
+
+struct inputfsm
+{
+    string input;
+    vector <string> outputs;
+    
+    inputfsm (int argc, char** argv)
+    {
+        Context ctx {new Begin};
+        
+        for (char** it = argv + 1; it < argv + argc; ++it)
+        {
+            ctx.process (*it);
+        }
+        
+        input = ctx.input;
+        outputs = ctx.outputs;
+    }
+};
--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,32 @@
 using namespace std;
+#include "inputfsm.hpp"
 
-int main(int argc, char const *argv[])
+#if defined (Debug)
+auto main (int,  char**) -> int
 {
-    cout << "hi" << endl;
+    int argc = 5;
+    char** argv = new char*[argc]{new char[]{}, new char[]{"--input"}, new char[]{"/Users/philipwenkel/GitHub/phan/tests/test_phan_app/testFiles_pre/1.hpp"}, new char[]{"--output"}, new char[]{"/Users/philipwenkel/GitHub/phan/tests/test_phan_app/testFiles_post/1.hpp"}};
+    
+#elif defined (Release)
+auto main (int argc,  char** argv) -> int
+{
+#endif
+
+    
+        
+    
+    
+    
+    
+    
+    
+    auto [input_file, output_files] = inputfsm (argc, argv);
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
